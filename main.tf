84f3a628ae0238e86be63c51714250d9318ad598
kovvurusubbu <99384027+kovvurusubbu@users.noreply.github.com>
2023-04-09 01:37:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+https://jhooq.com/prometheous-k8s-aws-setup/
+https://jhooq.com/prometheous-k8s-aws-setup/ helm 
+https://computingforgeeks.com/setup-prometheus-and-grafana-on-kubernetes/
+
+
+
+
+kubectl apply -f kube-state-metrics-configs/
+kubectl get deployments kube-state-metrics -n kube-system
+
+
+kubectl get deployments kube-state-metrics -n kube-system
+git clone https://github.com/devopscube/kube-state-metrics-configs.git
+kubectl apply -f kube-state-metrics-configs/
+kubectl get deployments kube-state-metrics -n kube-system
